d14d718e20995d5a2e35bad50930b5a28a01b4d7
Daniel Winklehner <winklehn@mit.edu>
2019-04-12 19:09:34 -0400

    playing with background colors in graph -DW

--- py_electrodes/py_electrodes.py
+++ py_electrodes/py_electrodes.py
@@ -377,11 +377,9 @@ class PyElectrode(object):
             transform_str = """SetFactory("OpenCASCADE");
 Geometry.NumSubEdges = 100; // nicer display of curve
 Merge "{}";
-
 v() = Volume "*";
-
 Translate {{ {}, {}, {} }} {{ Volume{{v()}}; }}
-Rotate {{ {{ {} {} {} }}, {{ 0, 0, 0 }}, {} }} {{  v(); }}
+Rotate {{ {{ {}, {}, {} }}, {{ 0, 0, 0 }}, {} }} {{  Volume{{v()}}; }}
                 """.format(self._orig_file, tx, ty, tz, v_rot[0], v_rot[1], v_rot[2], angle)
 
             transform_fn = os.path.join(TEMP_DIR, "{}_trafo.geo".format(self._id))

d742e7db84a8cce1f91328aff51bac14b2d0ec0f
Daniel Winklehner <winklehn@mit.edu>
2019-04-16 14:09:53 -0400

    fixing transformations -DW

--- py_electrodes/py_electrodes.py
+++ py_electrodes/py_electrodes.py
@@ -387,7 +387,7 @@ class PyElectrode(object):
             transform_str = """v() = Volume "*";
 {}Translate {{ {}, {}, {} }} {{ Volume{{v()}}; }}
 {}Rotate {{ {{ {}, {}, {} }}, {{ 0, 0, 0 }}, {} }} {{  Volume{{v()}}; }}
-""".format(self._orig_file, omit_t, tx, ty, tz, omit_r, v_rot[0], v_rot[1], v_rot[2], angle)
+""".format(omit_t, tx, ty, tz, omit_r, v_rot[0], v_rot[1], v_rot[2], angle)
 
             transform_fn = os.path.join(TEMP_DIR, "{}_trafo.geo".format(self._id))
             with open(transform_fn, "w") as _of:
